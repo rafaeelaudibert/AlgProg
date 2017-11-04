39c25bde05237465acab45daac922d389fa5ebfd
RafaAudibert <32079912+rafaeelaudibert@users.noreply.github.com>
2017-11-03 23:43:19 -0200

    Otimização de código
    Reduzido tamanho do código, transformando 2 funções em uma só
    
    Otimização de memória

--- labirinto.c
+++ labirinto.c
@@ -9,12 +9,7 @@
 void gotoxy(int, int);
 void textcolor(int);
 
-int const WIDTH_SCREEN = 100;
-int const HEIGHT_SCREEN = 30;
-
-void clrscr();
-
-//definição estruturas
+//Definição das estruturas
 struct
 {
     int x;
@@ -27,165 +22,115 @@ struct
     ghost unid[5];
 } typedef ghosts;
 
-struct
-{
-    int x;
-    int y;
-} typedef pacmanPos;
-
-struct
-{
-    int x;
-    int y;
-} typedef coord;
-
 //instâncias globais dos objetos
 ghosts fantasmas;
-pacmanPos pacman = {1,1};
-coord pastilhas[2000]; //Setado para um maximo de 2000 Pastilhas (Provavelmente não será maior do que 1500, embora o maximo possivel seja 2994)
-coord superPastilhas[4]; //Setado para um maximo de 4 Super Pastilhas
+extern int HEIGHT;
+extern int WIDTH;
 
 
-// Função que lê o arquivo labirinto.txt e configura a matriz do labirinto
-// retorna 1 se houve algum erro
-// retorna 0 se tudo ocorreu corretamente
-int readLab(char lab[30][100])
+//Inicia e carrega as estruturas, além de mostrar o labirinto na tela
+int showLab(char lab[HEIGHT][WIDTH], int *qtdePastilhas, int *pacmanx, int *pacmany)
+{
+
+    int i, j; //Contador do laço da matriz
+    int q_fantasmas=0, q_pastilhas=0; //Contagem de objetos
+
+    //Chamada das outras duas funções responsáveis pelo labirinto
+    if(readLab(lab))
+    {
+        return 1;
+    }
+
+    //Printagem do labirinto na tela
+    for(i=0; i<HEIGHT; i++)
+    {
+        for(j=0; j<WIDTH; j++)
+        {
+            switch(lab[i][j])
+            {
+            case 'c':
+            case 'C':
+                printf(" ");
+                *pacmanx=j+1;
+                *pacmany=i+1;
+                lab[i][j] = ' ';
+                break;
+            case 'w':
+            case 'W': // Fantasmas
+                printf(" ");
+                fantasmas.unid[q_fantasmas].x = j;
+                fantasmas.unid[q_fantasmas].y = i;
+                q_fantasmas++;
+                lab[i][j] = ' ';
+                break;
+            case '#':
+                textcolor(15); //BRANCO
+                printf("#");
+                break;
+            case 'o':
+                textcolor(14); //AMARELO
+                printf("o");
+                q_pastilhas++;
+                break;
+            case '*':
+                textcolor(13); //ROSA
+                printf("*");
+                break;
+            default:
+                printf(" ");
+                break;
+            }
+        }
+
+        if(i<HEIGHT-1)
+        {
+            printf("\n"); //Linha nova no final da linha
+        }
+
+    }
+
+    fantasmas.quant=q_fantasmas;
+
+    *qtdePastilhas=q_pastilhas; //Seta a quantidade total de pastilhas
+
+    return 0;
+}
+
+//Função que lê o arquivo labirinto.txt e configura a matriz do labirinto
+//Retorna 1 se houve algum erro
+int readLab(char lab[HEIGHT][WIDTH])
 {
     char str[101];
+    int i=0, j=0;
     FILE *arq;
 
-    // ponteiro para leitura do arquivo no 'endereco'
+    //Ponteiro para leitura do arquivo no 'endereco'
     arq = fopen("data/labirinto.txt", "r");
 
-    // se não conseguiu ler o arquivo, sai e retorna 0
+    //Se não conseguiu ler o arquivo, sai e retorna 1
     if(arq == NULL)
     {
         printf("ERROR: nao foi possivel abrir o arquivo\n");
         return 1;
     }
 
-    // i = linha, j = coluna
-    int i=0, j=0;
-    // lê cada uma das linhas do arquivo
-    while( fgets(str, 101, arq) != NULL || i<30)
+
+    //Lê cada uma das linhas do arquivo
+    while(fgets(str, 101, arq) != NULL || i<30)
     {
-        // se a leitura retornar uma linha em branco, ignora
+        //Se a leitura retornar uma linha em branco, ignora
         if(str[0] != '\n')
         {
-            // senão, adiciona a matriz lab a linha
+            //Senão, adiciona à matriz lab uma linha
             for(j=0; j<100; j++)
             {
                 lab[i][j] = str[j];
             }
-            // próxima linha da matriz
+
             i++;
         }
     }
 
     return 0;
-}
-
-int showLab(char lab[30][100])
-{
-    if(readLab(lab)){
-        return 1;
-    }
-    iniciaLabirinto(lab);
-
-    int i, j;
-
-    gotoxy(0,1);
-
-    //Printagem do labirinto na tela
-    for(i=0; i<30; i++)
-    {
-        for(j=0; j<100; j++)
-        {
-            if(lab[i][j]=='#')
-            {
-                textcolor(15);
-                printf("%c", lab[i][j]);
-            }
-            else if(lab[i][j]=='o')
-            {
-                textcolor(14);
-                printf("%c", lab[i][j]);
-            }
-            else if(lab[i][j]=='*'){
-                textcolor(13);
-                printf("%c", lab[i][j]);
-            }else
-            {
-                textcolor(0);
-                printf(" ");
-            }
-            textcolor(15);
-        }
-        if(i<29)
-        {
-            printf("\n");
-        }
-    }
-
-    return 0;
-}
-
-// inicia e carrega as structs
-void iniciaLabirinto(char lab[30][100])
-{
-    int q_fantasmas=0, q_pastilhas=0, q_superPastilhas=0;
-    // mode já deve estar configurado
-
-    int i, j;
-
-    // Carrega a localização de cada um dos objetos, e remove eles do labirinto, se necessario
-    for(i=0; i<30; i++)
-    {
-        for(j=0; j<100; j++)
-        {
-            switch(lab[i][j])
-            {
-            case 'c':
-            case 'C': // PacMan
-                pacman.x = j;
-                pacman.y = i;
-                lab[i][j] = ' ';
-                break;
-            case 'w':
-            case 'W': // Fantasmas
-                fantasmas.unid[q_fantasmas].x = j;
-                fantasmas.unid[q_fantasmas].y = i;
-                q_fantasmas++;
-                lab[i][j] = ' ';
-                break;
-            case '*': // Super-Pastilhas
-                superPastilhas[q_superPastilhas ].x = j;
-                superPastilhas[q_superPastilhas ].y = i;
-                q_superPastilhas++;
-                break;
-            case 'o': // Pastilhas
-                pastilhas[q_pastilhas].x = j;
-                pastilhas[q_pastilhas].y = i;
-                q_pastilhas++;
-                break;
-            }
-        }
-    }
-
-    fantasmas.quant=q_fantasmas;
-
-    return;
-}
-
-int retornaXPacman(void) //Retorna a posição x do pacman no labirinto, para utilizar na função main
-{
-
-    return pacman.x+1;
-}
-
-int retornaYPacman(void) //Retorna a posição y do pacman no labirinto, para utilizar na função main
-{
-
-    return pacman.y+1;
 }
 
--- labirinto.h
+++ labirinto.h
@@ -1,19 +1,14 @@
 #ifndef COMMANDS_H_
 #define COMMANDS_H_
 
+
 // Cabeçalho das funções
 
 // Função que lê o labirinto do endereco e coloca na matriz lab
 int readLab(char lab[30][100]);
 
 // Função que imprime o lab original
-int showLab(char lab[30][100]);
+int showLab(char lab[30][100], int*, int*, int*);
 
-// Carrega o labirinto e carrega as structs
-void iniciaLabirinto(char lab[30][100]);
-
-//Funções que retornam posição do pacman para as funções do arquivo main.c (Bugadas no momento)
-int retornaXPacman(void);
-int retornaYPacman(void);
 
 #endif
